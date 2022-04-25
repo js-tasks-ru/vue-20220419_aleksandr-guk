ff84ffd064363f0a76bec1256b4beeaca1776321
Alexander Guk <alexander-v-guk@yandex.ru>
2022-04-25 16:06:43 +0300

    Solve 01-02-basics vue calculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,31 @@
 // import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+import {createApp} from './vendor/vue.esm-browser.js';
+
+createApp({
+  data() {
+    return {
+      sum: 'Incorrect input',
+      firstNum: 0,
+      secondNum: 0,
+      calcMethods: {
+        sum: (firstNum, secondNum) => firstNum + secondNum,
+        subtract: (firstNum, secondNum) => firstNum - secondNum,
+        multiply: (firstNum, secondNum) =>  firstNum * secondNum,
+        divide: (firstNum, secondNum) =>  firstNum / secondNum,
+      }
+    }
+  },
+  methods: {
+    showChange() {
+      const checkedRadio = document.querySelector('input:checked');
+      if ( checkedRadio ) {
+         const method = checkedRadio.value;
+         if ( this.calcMethods[method] )  {
+           this.sum = this.calcMethods[method](this.firstNum, this.secondNum)
+         }
+      }
+    }
+  }
+}).mount('#app')
