fdb6f16e6db4343bd3d7c9e31f23e60d5cc5d963
Alexander Guk <alexander-v-guk@yandex.ru>
2022-04-27 17:36:41 +0300

    Resolve issues from comments

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,31 +1,30 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
-
-// Создайте Vue приложение
 import {createApp} from './vendor/vue.esm-browser.js';
 
 createApp({
   data() {
     return {
-      sum: 'Incorrect input',
       firstNum: 0,
       secondNum: 0,
-      calcMethods: {
-        sum: (firstNum, secondNum) => firstNum + secondNum,
-        subtract: (firstNum, secondNum) => firstNum - secondNum,
-        multiply: (firstNum, secondNum) =>  firstNum * secondNum,
-        divide: (firstNum, secondNum) =>  firstNum / secondNum,
-      }
+      operand: 'sum'
+    }
+  },
+  computed: {
+    result() {
+      return this[this.operand]();
     }
   },
   methods: {
-    showChange() {
-      const checkedRadio = document.querySelector('input:checked');
-      if ( checkedRadio ) {
-         const method = checkedRadio.value;
-         if ( this.calcMethods[method] )  {
-           this.sum = this.calcMethods[method](this.firstNum, this.secondNum)
-         }
-      }
+    sum() {
+      return this.firstNum + this.secondNum;
+    },
+    subtract() {
+      return this.firstNum - this.secondNum;
+    },
+    multiply() {
+      return this.firstNum * this.secondNum;
+    },
+    divide() {
+      return this.firstNum / this.secondNum;
     }
   }
 }).mount('#app')
